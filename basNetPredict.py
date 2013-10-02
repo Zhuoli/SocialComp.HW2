a0fa918c0e356085af759341542ce81f55ca92fa
Zhuoli <zhuoli@zhuosMac.local>
2013-10-01 20:01:21 -0400

    first day's upload

---
+++ basNetPredict.py
@@ -0,0 +1,103 @@
+import sys, random
+import networkx as nx
+import matplotlib.pyplot as plt
+def file2edgelist(argv):
+  if len(argv) < 3:
+      print 'Usage:', argv[0], '<graph file> <# of edges to predict>'
+      return
+
+  try:
+      graphfile = open(argv[1])
+  except:
+      print 'Error: Unable to open graph file.'
+      sys.exit()
+
+  try:
+      num_edges = int(argv[2])
+  except:
+      print 'Error: Unable to parse # of edges from command line'
+      sys.exit()
+
+  nodes = set()
+  edgelist = [] 
+  for line in graphfile:
+      try:
+          n1, n2 = line.split()
+      except:
+          print 'Error: Malformed graph file.'
+          return
+      # Do duplicate links handling
+      edgelist.append([n1,n2])
+      nodes.add(n1)
+      nodes.add(n2)
+  #for i in range(num_edges):
+  #    n1, n2 = random.sample(nodes, 2)
+  #    print '%s\t%s' % (n1, n2)
+  return edgelist, nodes
+
+# test usage
+def readfile():
+  edgelist,nodes = file2edgelist(['basnet','model2-2.txt','3'])
+  return edgelist
+#file2edgelist(sys.argv)
+
+
+# edgelist 2 Graph
+def edgelist2graph(edgelist):
+  G = nx.DiGraph()
+  G.add_edges_from(edgelist)
+  return G
+#return ether strongly connected components
+def getSubGraph(graph):
+  listOfComponentNodes4ECC= nx.weakly_connected_components(graph)
+  listOfComponentNodes4SCC = nx.strongly_connected_components(graph)
+  #strongly connected components
+  scc = []
+  for i in range(0,len(listOfComponentNodes4SCC)):
+    subgraph = graph.subgraph(listOfComponentNodes4SCC[i]) 
+    scc.append(subgraph)
+  # Weakly connceted components
+  ecc = []
+  for i in range(0,len(listOfComponentNodes4ECC)):
+    subgraph = graph.subgraph(listOfComponentNodes4ECC[i])
+    ecc.append(subgraph)
+  print 'graph nodes size:        ' + str(len(graph.nodes()))
+  print 'SCC   components number: ' + str(len(scc))
+  print 'ECC   components number: ' + str(len(ecc))
+  return scc,ecc
+
+
+#draw graph
+def draw(graph):
+  pos = nx.graphviz_layout(graph,prog='twopi',args='')
+  plt.figure(figsize=(8,8))
+  nx.draw(graph,pos,node_size=100,alpha=0.1,node_color='blue',with_labels=True)
+  plt=axis=('equal')
+  plt.savfig('sub_graph.png')
+  plt.show()
+
+
+# get prediction in SCC
+def getPredictionInSCC(scc,number):
+  # put the likely connected links to prediction
+  prediction = []
+  for subgraph in range(0,len(scc)):
+    triadicClosurePrediction = getTriadicClosurePrediction(subgraph)
+    if len(triadicClosurePrediction) > 0:
+      prediction.append(triadicClosurePrediction)
+  return prediction
+
+# ge prediction in WCC
+def getPredictionInWCC(scc,number):
+  # put the likely connected links to prediction
+  prediction = []
+  for subgraph in range(0,len(scc)):
+    triadicClosurePrediction = getTriadicClosurePrediction(subgraph)
+    if len(triadicClosurePrediction) > 0:
+      prediction.append(triadicClosurePrediction)
+  return prediction
+
+# get the Triadic Closure Prediction
+def getTriadicClosurePrediction(graph):
+  predition = []
+  return prediction
