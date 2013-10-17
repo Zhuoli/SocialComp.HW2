0d1cede4128be09bc0e955c8eb9138bbdcdf6da5
ZhuoliLiang <342916739@qq.com>
2013-10-16 20:45:44 -0400

    now runs much faster after removing networkx lib

--- basNetPredict.py
+++ basNetPredict.py
@@ -2,10 +2,8 @@ import sys, random
 import networkx as nx
 from collections import namedtuple
 from numpy import array
-import multiprocess as process
-import time
 #import matplotlib.pyplot as plt
-def file2edgelist(argv):
+def parsefile(argv):
   if len(argv) < 3:
       print 'Usage:', argv[0], '<graph file> <# of edges to predict>'
       sys.exit()
@@ -24,6 +22,10 @@ def file2edgelist(argv):
 
   nodes = set()
   edgelist = [] 
+  inHash = {}
+  outHash = {}
+  nbrsHash = {}
+  edgeHash = {}
   for line in graphfile:
       try:
           n1, n2 = line.split()
@@ -31,13 +33,17 @@ def file2edgelist(argv):
           print 'Error: Malformed graph file.'
           return
       # Do duplicate links handling
+      edge = [n1,n2]
+      edgeHash[tuple(edge)] = True
+      outHash[n1] = [n2]
+      inHash[n2] = [n1]
       edgelist.append([n1,n2])
       nodes.add(n1)
       nodes.add(n2)
   #for i in range(num_edges):
   #    n1, n2 = random.sample(nodes, 2)
   #    print '%s\t%s' % (n1, n2)
-  return edgelist, nodes
+  return  nodes,edgeHash,outHash,inHash
 
 # test usage
 def readfile():
@@ -47,10 +53,18 @@ def readfile():
 
 
 # edgelist 2 Graph
-def edgelist2graph(edgelist):
-  G = nx.DiGraph()
-  G.add_edges_from(edgelist)
-  return G
+def edgelist2graph(edgelist,inHash,outHash,nbrsHash,edgeHash):
+  for edge in edgelist:
+    edgeHash[tuple(edge)] = 1
+    #get out link hash table
+    neighbors = outHash[edge[0]]
+    neighbors.append(edge[1])
+    # get in link hash Table
+    neighbors = inHash[edge[1]]
+    neighbors.append(edge[1])
+  return 
+
+
 #return ether strongly connected components
 def getSubGraph(graph):
   listOfComponentNodes4ECC= nx.weakly_connected_components(graph)
@@ -94,13 +108,8 @@ def debugPrints(pairs):
 
 # Author: Zhuoli
 # make Prediction  using Jaccard's coefficient method
-def predictorAtCoefficient(graph,starttime):
-  print 'getNeighborHash time: ' + str(time.time() - starttime)
-  nbrsInHash,nbrsOutHash,nbrs = getNeighborHash(graph)
-  print 'getEdgeHash time: ' + str(time.time() - starttime)
-  edgeHash = getEdgeHash(graph)
-  print 'deliverTask time: ' + str(time.time() - starttime)
-  items = process.deliverTask(starttime,graph,nbrsOutHash,nbrsInHash,nbrs,edgeHash)
+def predictorAtCoefficient(nodes,edgeHash,nbrsInHash,nbrsOutHash):
+  items = predictWithNeighborsOverLapRate(nodes,edgeHash,nbrsOutHash,nbrsInHash)
   return items
 # Author: Zhuoli
 # fullfill the neighbor hash sable
@@ -136,6 +145,58 @@ def getEdgeHash(community):
   for edge in edges:
     edgeHash[tuple(edge)] = 1
   return edgeHash
+# Author: Zhuoli
+# make prediction with neighbors overlap
+# rate method in a undirected community
+def predictWithNeighborsOverLapRate(nodes,edgeHash,nbrsOutHash,nbrsInHash):
+#  print 'in predict with neighbor over lap rate'
+  visitedHash ={}
+  result = {}
+  nbrs = {}
+  k = 1
+  for node in nodes:
+    outNeighbors = nbrsOutHash.get(node)
+    if outNeighbors == None:
+        outNeighbors = []
+    nbrsOutHash[node] = list(set(outNeighbors))
+    inNeighbors = nbrsInHash.get(node)
+    if inNeighbors == None:
+        inNeighbors = []
+    nbrsInHash[node] = list(set(inNeighbors))
+    outset = set(nbrsOutHash[node])
+    inset = set(nbrsInHash[node])
+    nbrs[node] = list(outset | inset)
+  for node in nodes:
+    #print 'loop: ' + str(k) +'\t' + '%.3f%% completed' % ((k*100 + 0.0)/ 97134) 
+    k = k+1
+    getPred4ThisNode(node,visitedHash,result,edgeHash,nbrsOutHash,nbrsInHash,nbrs)
+  return result.items()
+
+# get prediction for this node
+def getPred4ThisNode(node,visitedHash,result,edgeHash,nbrsOutHash,nbrsInHash,nbrs):
+  visitedHash[node] = True
+  nodeNeighbors = list(set(nbrs[node]))
+#   print 'first loop level node neighbors size: ' + str(len(nodeNeighbors))
+  for nodeNeighbor in nodeNeighbors:
+    subneighbors = nbrs[nodeNeighbor]
+#   print 'second loop level subneighbors size: ' + str(len(subneighbors))
+    for subneighbor in subneighbors:
+#        print 'third loop level node size: ' + str(len(subneighbors))
+        # omit connected links
+      if subneighbor == node:
+        continue
+      if subneighbor in visitedHash:
+        continue
+      if tuple([subneighbor,node]) in edgeHash:
+        continue
+      if tuple([node,subneighbor]) in edgeHash:
+        continue
+      numerator = (len(set(nbrs[subneighbor]) & set(nodeNeighbors)) + 0.0)
+      denominator = (len(set(nbrs[subneighbor]) | set(nodeNeighbors)) + 0.1)
+      rate = numerator / denominator
+      edge = tuple([subneighbor,node])
+      result[edge] = rate
+  return
 # Author: Zhuoli
 # make prediction using common neighbors method
 def predictorAtCommonNeighbors(communities):
